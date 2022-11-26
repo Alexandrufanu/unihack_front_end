81c228d04e08c8a939d27e69db86f65b778a512a
Alexfanu <78355480+Alexfanu@users.noreply.github.com>
2022-11-27 00:30:06 +0200

    added a loginfile

---
+++ front-end/src/Login.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function LogIn() {
+  return (
+    <div>
+        <button>
+            AAAAA
+        </button>
+      
+    </div>
+  );
+}
+
+export default LogIn;
