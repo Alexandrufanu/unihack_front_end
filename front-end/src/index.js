a4fe823d1409b02a347640466c217dbbe5f964d9
Tomus Nicolas <nicolas.tomus@student.upt.ro>
2022-11-26 17:02:17 +0200

    Upload the landing page

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -11,6 +11,24 @@ root.render(
   </React.StrictMode>
 );
 
+
+let header = document.getElementById('header');
+
+document.addEventListener('scroll', function() {
+    // Get scroll position
+    let scrollPosition = window.pageYOffset;
+
+    // Calculate whether the scroll it 350px or not then set the opacity to face the header out
+    if (scrollPosition <= 350) {
+        header.style.opacity = 1 - scrollPosition / 350;
+    } else {
+        header.style.opacity = 0;
+    }
+
+});
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
