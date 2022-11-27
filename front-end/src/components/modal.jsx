d1db05cde5d87f72230d6274b4a223b5264414eb
Tomus Nicolas <nicolas.tomus@student.upt.ro>
2022-11-27 11:15:15 +0200

    Sing In

---
+++ front-end/src/components/modal.jsx
@@ -0,0 +1,39 @@
+// import './modal.css';
+/*
+import React, { useState } from 'react';
+//import Button from 'react-bootstrap/Button';
+//import Modal from 'react-bootstrap/Modal';
+import '../index.css';
+
+
+function LogInModal() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow} class = "Sign_In" >
+        Sign In
+      </Button>
+      
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default LogInModal;*/
