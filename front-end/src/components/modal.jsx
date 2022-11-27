0a584432fc81033b7ddc2c16eb5b4ab0ce8a8b1a
Tomus Nicolas <nicolas.tomus@student.upt.ro>
2022-11-27 11:53:10 +0200

    Sing In Alpha

--- front-end/src/components/modal.jsx
+++ front-end/src/components/modal.jsx
@@ -1,15 +1,8 @@
 // import './modal.css';
-<<<<<<< HEAD
 /*
 import React, { useState } from 'react';
 //import Button from 'react-bootstrap/Button';
 //import Modal from 'react-bootstrap/Modal';
-=======
-
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
->>>>>>> 2622e28b8e335d74d2c4b70591c6ad35377b687a
 import '../index.css';
 
 
@@ -24,11 +17,7 @@ function LogInModal() {
       <Button variant="primary" onClick={handleShow} class = "Sign_In" >
         Sign In
       </Button>
-<<<<<<< HEAD
       
-=======
-
->>>>>>> 2622e28b8e335d74d2c4b70591c6ad35377b687a
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
@@ -47,8 +36,4 @@ function LogInModal() {
   );
 }
 
-<<<<<<< HEAD
 export default LogInModal;*/
-=======
-export default LogInModal;
->>>>>>> 2622e28b8e335d74d2c4b70591c6ad35377b687a
